cef71f74fe36d748149150ede42692611feb3157
Brian <brian.musial@gmail.com>
2018-10-31 16:09:38 -0700

    adding node version

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 exports.DATABASE_URL =
   process.env.DATABASE_URL || "mongodb://localhost/repairBuddy-app";
 exports.TEST_DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost/test-repairBuddy-app";
+  process.env.mongoimport || "mongodb://localhost/test-repairBuddy-app";
 exports.PORT = process.env.PORT || 3000;
